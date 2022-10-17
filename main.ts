5bd0cd7be038b9ded88de1beec8f4cb78b79663b
TCF09ethan <115450502+TCF09ethan@users.noreply.github.com>
2022-10-17 14:15:09 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0 + 0)
+    count = 9
+    count += -1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(0)
+    basic.showNumber(9)
+    basic.showNumber(8)
+    basic.showNumber(7)
+    basic.showNumber(6)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+})
 basic.forever(function () {
     basic.showNumber(1)
     basic.showNumber(2)
